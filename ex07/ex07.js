4852df2f215cc253d720029df702a0eda2befe23
camillaUnicap <101574737+camillaUnicap@users.noreply.github.com>
2022-04-07 20:56:22 -0300

    Add files via upload

--- ex07/ex07.js
+++ ex07/ex07.js
@@ -8,32 +8,32 @@ function gerar() {
         switch (i) {
             case 0:
                 numeros = numero1 + numero2;
-                linha.innerHTML += ` ${numeros} `;
+                linha.innerHTML += `Soma: ${numeros} `;
                 linha.innerHTML += "\n";
                 break;
             case 1:
                 numeros = numero1 - numero2;
-                linha.innerHTML += ` ${numeros} `;
+                linha.innerHTML += `Subtração: ${numeros} `;
                 linha.innerHTML += "\n";
                 break;
             case 2:
                 numeros = numero1 * numero2;
-                linha.innerHTML += ` ${numeros} `;
+                linha.innerHTML += `Multiplicação: ${numeros} `;
                 linha.innerHTML += "\n";
                 break;
             case 3:
                 numeros = numero1 / numero2;
-                linha.innerHTML += ` ${numeros} `;
+                linha.innerHTML += `Divisão: ${numeros} `;
                 linha.innerHTML += "\n";
                 break;
             case 4:
                 numeros = numero1 % numero2;
-                linha.innerHTML += ` ${numeros} `;
+                linha.innerHTML += `Resto: ${numeros} `;
                 linha.innerHTML += "\n";
                 break;
             case 5:
                 numeros = numero1 ** numero2;
-                linha.innerHTML += ` ${numeros} `;
+                linha.innerHTML += `Potenciação: ${numeros} `;
                 linha.innerHTML += "\n";
                 break;
             default:
